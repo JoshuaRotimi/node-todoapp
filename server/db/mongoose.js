8363fb1c1deaa8c1faeab72f346498f644add592
JoshuaRotimi <62189959+JoshuaRotimi@users.noreply.github.com>
2020-06-02 23:00:57 +0100

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,3 +4,5 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp').then(() => console.log('Connected!'));
 
 module.exports = {mongoose};
+
+// mongodb+srv://joshuarotimi:<password>@cluster0-kydeo.mongodb.net/test?retryWrites=true&w=majority
